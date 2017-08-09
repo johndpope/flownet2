quaternion normalization corrected which effects rta loss

src/euromav.py
@@ -4,12 +4,19 @@ from flownet2.flownet2 import FlowNet2
 from flownet_c.flownet_c import FlowNetC
 import numpy as np
 from extras import ominus,spinning_cursor
-from losses import safe_rtLoss
+from losses import safe_rtLoss,rtLoss
 from hyperparams import *
 from dump2disk import *
+import scipy.io
 
 spinner = spinning_cursor() #just for cool ouput
 details()# print all the details
+
+prev_point=np.array([[0],[0],[0],[1]],dtype=np.float32)
+gt=np.array([])
+pred_traj=np.array([])
+error_rtd=np.array([])
+error_rta=np.array([])
 
 slim = tf.contrib.slim
 # Create a new network
@@ -29,8 +36,8 @@ else:
 	writer_t = tf.summary.FileWriter(train_log_dir, None)
 	writer_v = tf.summary.FileWriter(validation_log_dir, None)
 
-(i_t1,i_t2,p_t1,p_t2)=bat.euromav_batch(train_txt,batch_size)#configs from dataset_configs
-(i_v1,i_v2,p_v1,p_v2)=bat.euromav_batch(val_txt,batch_size)#configs from dataset_configs
+(il_t1,il_t2,ir_t1,ir_t2,p_t1,p_t2)=bat.euromav_batch(train_txt,batch_size,shuffle=False)#configs from dataset_configs
+(il_v1,il_v2,ir_v1,ir_v2,p_v1,p_v2)=bat.euromav_batch(val_txt,batch_size,shuffle=False)#configs from dataset_configs
 
 rt12_g = ominus(p_t2, p_t1)
 
@@ -47,15 +54,16 @@ if (do_zero_motion):
 	 [0,1,0,0],
 	 [0,0,1,0],
 	 [0,0,0,1]]], dtype=tf.float32),[1,4,4]),[batch_size,1,1])
-	(rtd,rta) = safe_rtLoss(RT_e,rt12_g)
-	cost = (rta+rtd)
+	(rtd,rta) = rtLoss(RT_e,rt12_g)
+	cost = rta+(1.0*rtd)
 elif Mode=='train':
-	(rtd, rta) = safe_rtLoss(RT_e, rt12_g)
-	cost = (rta+rtd)
+	(rtd, rta) = rtLoss(RT_e, rt12_g)
+	cost = rta+(1.0*rtd)
 	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 else:
-	(rtd, rta) = safe_rtLoss(RT_e, rt12_g)
-	cost = (rta+rtd)
+	(rtd, rta) = rtLoss(RT_e, rt12_g)
+	cost = rta+(1.0*rtd)
+	
 
 with tf.name_scope("RT_loss"):
 		odo_loss_sum = tf.summary.scalar("loss", cost)
@@ -76,11 +84,11 @@ with tf.Session() as sess:
 	for i in range(0,max_iterations):
 		if (Mode=='train'):
 			# train dataset
-			i1_, i2_ = sess.run([i_t1,i_t2])
-			[opt, train_loss, summ1] = sess.run([optimizer, cost, summary],feed_dict={x: i1_, y: i2_})
+			i1_, i2_ = sess.run([il_t1,il_t2])
+			[opt, train_loss, summ1,RT_e_,rt12_g_] = sess.run([optimizer, cost, summary,RT_e,rt12_g],feed_dict={x: i1_, y: i2_})
 			writer_t.add_summary(summ1,i)
 			#validation dataset
-			i1_, i2_ = sess.run([i_v1,i_v2])
+			i1_, i2_ = sess.run([il_v1,il_v2])
 			[val_loss, summ2] = sess.run([cost, summary],feed_dict={x: i1_, y: i2_})
 			sys.stdout.write("\r")
 			sys.stdout.write(spinner.next())
@@ -90,6 +98,8 @@ with tf.Session() as sess:
 			if (i%10000 ==0):
 				save_path = saver1.save(sess, save_dir)
 				print("Model saved in file: %s at step %d" % (save_path,i))
+				print("predicted T:",RT_e_[0])
+				print("ground truth T:",rt12_g_[0])
 
 		elif do_zero_motion:
 			# train dataset
@@ -105,14 +115,17 @@ with tf.Session() as sess:
 			sys.stdout.flush()
 			writer_v.add_summary(summ2,i)
 		else:
-			i1_, i2_ = sess.run([i_v1,i_v2])
-			[test_loss, summ1,rta_,rtd_] = sess.run([cost, summary,rta,rtd],feed_dict={x: i1_, y: i2_})
+			i1_, i2_ = sess.run([il_t1,il_t2])
+			[test_loss, summ1,rta_,rtd_,RT_e_,rt12_g_] = sess.run([cost, summary,rta,rtd,RT_e,rt12_g],feed_dict={x: i1_, y: i2_})
 			writer_test.add_summary(summ1,i)
 			sys.stdout.write("\r")
 			sys.stdout.write(spinner.next())
+			# new_point_=np.matmul(RT_e_,prev_point)
 			sys.stdout.write('  iteration:%d test loss:%f, rta loss:%f,rtd loss:%f'%(i,test_loss,rta_,rtd_))
+			# print(new_point_,p_t1_[0,0:3,3])
+			print('pred_=',np.reshape(RT_e_[0,0:3,3],[1,3]),'gt=',np.reshape(rt12_g_[0,0:3,3],[1,3]))
 			sys.stdout.flush()
-			if (i%10000 ==0):
-				print "Dumping few visuals" 
-				dump2disk(vis_dir, 1, i1_,i2_)
+			# if (i%10000 ==0):
+			# 	print "Dumping few visuals" 
+			# 	dump2disk(vis_dir, 1, i1_,i2_)
 	print('optimization finished')

src/extras.py
@@ -56,7 +56,7 @@ def quat2rotmat(q):#tf array [qw,qx,qy,qz] as the data is in this format
     q3=tf.reshape(q[:,3],[bs,1])
     #normalization
     z=tf.concat([tf.square(q0),tf.square(q1),tf.square(q2),tf.square(q3)],1)
-    #z=tf.sqrt(tf.reduce_sum(z, 1, keep_dims=True)) 
+    z=tf.sqrt(tf.reduce_sum(z, 1, keep_dims=True)) #only this normalization gives orthonormal matrix
     z=tf.reduce_sum(z, 1, keep_dims=True)
     q0=tf.div(q0,z)
     q1=tf.div(q1,z)
@@ -80,9 +80,14 @@ def quat2rotmat(q):#tf array [qw,qx,qy,qz] as the data is in this format
     # R=tf.array([])
 def pose2mat(p):
     shape = p.get_shape()
-    bs = int(shape[0])
+    bs = int(shape[0])  
     q=p[:,3:7]
+
     t=tf.reshape(p[:,0:3],[bs,3])
+    # z=tf.concat([tf.square(tf.reshape(t[:,0],[bs,1])),tf.square(tf.reshape(t[:,1],[bs,1])),tf.square(tf.reshape(t[:,2],[bs,1]))],1)
+    # z=tf.reduce_sum(z, 1, keep_dims=True)
+    # t=tf.div(t,z)
+
     R=quat2rotmat(q)
     T=merge_rt(R,t)
     return T

src/hyperparams.py
@@ -16,9 +16,9 @@ pretrained_flow=True
 archi='Flownetc'
 quater=True
 fine_tune=False
-do_avgpooling=False
+do_avgpooling=True
 if not do_zero_motion:
-	Mode='test'
+	Mode='train'
 
 #read tf records txt files
 train_txt='./train.txt'
@@ -76,6 +76,18 @@ rotation=np.array(c2_1[0:3,0:3], dtype=np.float32)
 translation=np.reshape(c2_1[0:3,3],[1,3])
 distortion_coefficients_2= np.array([-0.28368365,  0.07451284, -0.00010473, -3.55590700e-05])
 
+
+generate_trajectory=False
+if generate_trajectory:
+	max_iterations=100
+	batch_size=1
+	Mode='test'
+	train_txt='./records.txt'
+
+
+
+
+name='lr_e_4_'
 #log directories
 log_dir='./graphs/'
 if do_zero_motion:
@@ -83,8 +95,8 @@ if do_zero_motion:
 	validation_log_dir=log_dir+'zero_validation'
 if Mode=='train':
 	dir_='train_'+('2_' if archi=='Flownet2' else 'c_')+('sc_' if not quater else 'q_')+('avg' if do_avgpooling else '')+('_scratch' if not pretrained_flow else '')+('_fine/' if fine_tune else '/')
-	train_log_dir=log_dir+dir_
-	validation_log_dir=log_dir+'validation'+('2_' if archi=='Flownet2' else 'c_')+('sc_' if not quater else 'q_')+('avg' if do_avgpooling else '')+('_scratch' if not pretrained_flow else '')+('_fine/' if fine_tune else '/')
+	train_log_dir=log_dir+name+dir_
+	validation_log_dir=log_dir+name+'validation'+('2_' if archi=='Flownet2' else 'c_')+('sc_' if not quater else 'q_')+('avg' if do_avgpooling else '')+('_scratch' if not pretrained_flow else '')+('_fine/' if fine_tune else '/')
 if Mode=='test':
 	dir_='test_'+('2_' if archi=='Flownet2' else 'c_')+('sc_' if not quater else 'q_')+('avg' if do_avgpooling else '')+('_scratch' if not pretrained_flow else '')+('_fine/' if fine_tune else '/')
 	test_log_dir=log_dir+dir_
@@ -93,7 +105,7 @@ vis_dir='./vis/'+dir_
 #checkpoint name to save
 checkpoint_dir='./checkpoints/'
 if Mode=='train':
-	save_dir=checkpoint_dir+('last_layer_' if not fine_tune else 'fine_tune_')+('flownet2_' if archi=='Flownet2' else 'flownetc_')+('q_' if quater else 'sc_')+('_scratch' if not pretrained_flow else '')+('avg/' if do_avgpooling else 'fc/')+('last_layer.ckpt' if not fine_tune else 'fine_tune.ckpt')
+	save_dir=checkpoint_dir+('last_layer_' if not fine_tune else 'fine_tune_')+('flownet2_' if archi=='Flownet2' else 'flownetc_')+('q_' if quater else 'sc_')+('_scratch' if not pretrained_flow else '')+('avg' if do_avgpooling else 'fc')+(name+'/')+('last_layer.ckpt' if not fine_tune else 'fine_tune.ckpt')
 
 #checkpoint to load
 if (Mode=='train'): 
@@ -107,7 +119,11 @@ if (Mode=='train'):
 		checkpoint=checkpoint_dir+'last_layer_'+('flownet2_' if archi=='Flownet2' else 'flownetc_')+('q_' if quater else 'sc_')+('avg/' if do_avgpooling else 'fc/')+'last_layer.ckpt' 
 else:
 	variables_to_exclude=[]
+	# checkpoint=checkpoint_dir+'translation_flownetc_q_fc/'+'last_layer.ckpt'
+	# checkpoint=checkpoint_dir+'last_layer_flownetc_q_fconlyrotation/'+'last_layer.ckpt' 
 	checkpoint=checkpoint_dir+'last_layer_'+('flownet2_' if archi=='Flownet2' else 'flownetc_')+('q_' if quater else 'sc_')+('avg/' if do_avgpooling else 'fc/')+'last_layer.ckpt' 
+
+
 def details():
 	sys.stdout.write('-'*30)
 	sys.stdout.write('\n    %s     %s\n'%(Mode,archi))

src/losses.py
@@ -6,12 +6,15 @@ EPS = 1e-6
 
 def rtLoss(rt_e, rt_g):
     with tf.variable_scope("rtLoss"):
-        rt_eg = ominus(rt_e,rt_g)
-        rtd = tf.reduce_mean(compute_distance(rt_eg))
-        rta = tf.reduce_mean(compute_angle(rt_eg))
-        td = tf.reduce_mean(compute_t_diff(rt_e,rt_g))
-        ta = tf.reduce_mean(compute_t_ang(rt_e,rt_g))
-        return rtd, rta, td, ta
+        r_e,t_e = split_rt(rt_e)
+        r_g,t_g = split_rt(rt_g)
+        print_shape(r_e)
+        print_shape(r_g)
+        rt_eg = tf.matmul(tf.transpose(r_e,[0,2,1]),r_g)
+        # rt_eg = tf.matmul(tf.matrix_inverse(r_e),r_g)    
+        rtd = tf.reduce_mean(norm(t_e-t_g))
+        rta = tf.reduce_mean(compute_angle_3x3(rt_eg))
+        return rtd, rta
 
 def safe_rtLoss(rt_e, rt_g):
     with tf.variable_scope("rtLoss"):